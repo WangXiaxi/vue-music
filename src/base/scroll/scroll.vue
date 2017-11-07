c1b06653e6821bcc7fad0b25de6d95c539c8e5a2
WangXiaxi <995107408@qq.com>
2017-11-07 15:49:17 +0800

    add

---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,62 @@
+<template>
+  <div ref="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script type="text/javascript-6">
+  import BScroll from 'better-scroll'
+
+  export default {
+    props: {
+      probeType: {
+        type: Number,
+        default: 1
+      },
+      click: {
+        type: Boolean,
+        default: true
+      },
+      data: {
+        type: Array,
+        default: null
+      }
+    },
+    mounted () {
+      setTimeout(() => {
+        this._initScroll()
+      },20)
+    },
+    methods: {
+      _initScroll () {
+        if(!this.$refs.wrapper) {
+          return
+        }
+        this.scroll = new BScroll(this.$refs.wrapper, {
+          probeType: this.probeType,
+          click: this.click
+        })
+      },
+      enable () {
+        this.scroll && this.scroll.enable()
+      },
+      disenable () {
+        this.scroll && this.scroll.disenable()
+      },
+      refresh () {
+        this.scroll.refresh()
+      },
+    },
+    watch: {
+      data () {
+        setTimeout(() => {
+          this.refresh()
+        }, 20)
+      }
+    }
+  }
+</script>
+
+<style type="text/css">
+  
+</style>
