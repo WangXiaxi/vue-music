52b70f87920525c1e44afcc9f5b677b8358fad87
WangXiaxi <995107408@qq.com>
2017-12-20 10:08:41 +0800

    Signed-off-by: WangXiaxi <995107408@qq.com>

--- src/components/play-song/play-song.vue
+++ src/components/play-song/play-song.vue
@@ -1,34 +1,73 @@
 <template>
-  <div class="play-song" :style="ifShowStyle">
-    
+  <div class="play-song" v-show="playList.length>0">
+    <div class="normal-play" v-show="fullscreen">
+      <i class="goback" @click="closeFullscreen"></i>
+      <div class="play-tit">{{singInfo.name}}</div>
+    </div>
+    <div class="mini-play">
+      
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import {mapGetters} from 'vuex'
+  import {mapGetters, mapMutations} from 'vuex'
   export default {
     computed: {
-      ifShowStyle () {
-        let ifShow = this.playList.length > 0 ? 'block' : 'none'
-        return `display: ${ifShow}`
-      },
       ...mapGetters([
-        'playList'
+        'playList',
+        'fullscreen',
+        'singInfo'
       ])
     },
+    mounted () {
+      console.log(this.playList)
+    },
     methods: {
+      closeFullscreen () {
+        this.setFullscreen(false)
+      },
+      openFullscreen () {
+        this.setFullscreen(true)
+      },
+      ...mapMutations({
+        setFullscreen: 'SET_FULLSCREEN'
+      })
     }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  .play-song
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+  
+  .normal-play
     position: fixed
-    bottom: 0
+    top: 0
     left: 0
     right: 0
+    bottom: 0
+    z-index: 120
+    background: rgba(0, 0, 0, 0.4)
+    .goback
+      display: block
+      width: 40px
+      height: 40px
+      position: absolute
+      top: 0
+      left: 0
+      z-index: 100
+      background: 50% 50% no-repeat
+      bg-image('goback')
+      background-size: 10px
+      transform: rotate(-90deg)
+      
+  .mini-play
+    position: fixed
+    left: 0
+    right: 0
+    bottom: 0
+    z-index: 119
     height: 60px
-    background: red
-    z-index: 100
-    display: none
+    background: rgba(157, 145, 133, 0.4)
 </style>
--- src/store/getters.js
+++ src/store/getters.js
@@ -2,3 +2,4 @@ export const singer = state => state.singer
 export const singInfo = state => state.singInfo
 export const currentIndex = state => state.currentIndex
 export const playList = state => state.playList
+export const fullscreen = state => state.fullscreen
